1ad5ba0e1248e6bc32ba05fc5bc072894ec41e6a
Sami Kammoun <kammounsami77@gmail.com>
2023-01-07 22:25:01 +0100

    added port 8080

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const express = require("express");
 const app = express();
 const utils = require("./utils.js");
 
+const port = process.env.PORT || 3000;
+
 app.get("/", (req, res) => {
   res.send("Hello, World!");
 });
 
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log("Server listening on port 3000");
 });
 
